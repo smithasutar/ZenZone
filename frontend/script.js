7b9e14b320371e20fa281778925665196ed9746c
Smitha <sutarsmitha@gmail.com>
2025-10-16 14:46:39 -0400

    url update

--- frontend/script.js
+++ frontend/script.js
@@ -35,7 +35,7 @@ async function sendMessage(){
 
 
     try {
-        const response = await fetch('http://127.0.0.1:8000/chat', {
+        const response = await fetch('https://zenzone-pq9t.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message }),

aa5c9a53dde130af41d9051d5018d85464763b9f
Smitha <sutarsmitha@gmail.com>
2025-11-10 21:51:34 -0500

    minor changes

--- frontend/script.js
+++ frontend/script.js
@@ -34,7 +34,7 @@ async function sendMessage(){
     sendBtn.disabled=true;
 
     try {
-        const response = await fetch('https://zenzone-pq9t.onrender.com', {
+        const response = await fetch('http://127.0.0.1:8000/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message }),
